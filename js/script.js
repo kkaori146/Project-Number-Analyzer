29707f2c82923a44b1b0394afd1de62c85336545
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-05 09:11:32 +0900

    Higher and Smaller number

--- js/script.js
+++ js/script.js
@@ -25,19 +25,32 @@ function adicionar() {
    let item = document.createElement('option')
    item.text = `Valor ${num.value} adicionado.`
    lista.appendChild(item)
+   res.innerHTML = ''
 
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
     num.value= ''
-    num.focu() //apagou na função de cima e a função focu já deixa o cursor
+    num.focus() //apagou na função de cima e a função focu já deixa o cursor
 }
 function finalizar() {
 if (valores.length == 0) {
 window.alert('Adicione valores antes de finalizar!')
 } else {
     let tot = valores.length
+
+    let maior = valores [0]
+    let menor = valores [0]
+    for (let pos in valores) {
+        if (valores[pos] > maior)
+        maior = valores[pos]
+        if (valores [pos] < menor)
+        menor = valores [pos]
+    }
+
     res.innerHTML = ''
     res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
 }
 }
